635237ebfd3ea5c30f626049dd98a29bee4f3954
celeste <celesteascue25@gmail.com>
2026-02-10 13:16:31 +0100

    Primer Commit

--- src/principal.java
+++ src/principal.java
@@ -4,6 +4,8 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+			System.out.println("Hola Mundo");
+			System.out.println("Primera Versión");
 	}
 
 }
